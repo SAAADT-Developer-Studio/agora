023e08048f27177302425d64422af1440f6cae65
marko154 <marko.gartnar777@gmail.com>
2025-02-24 00:40:14 +0100

    rename

--- scraper/app/providers/vecer.py
+++ scraper/app/providers/vecer.py
@@ -8,8 +8,8 @@ BASE_URL = "https://vecer.com"
 async def fetch_articles():
     start_date = (datetime.now() - timedelta(days=1)).date()
     end_date = datetime.now().date()
-    n_pages = 50
-    url = f"{BASE_URL}/rubrika/danes-objavljeno/1/{n_pages}/{start_date} 00:00:00/{end_date} 23:59:59"
+    page_size = 50
+    url = f"{BASE_URL}/rubrika/danes-objavljeno/1/{page_size}/{start_date} 00:00:00/{end_date} 23:59:59"
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
         articles = response.json()
