79aceed147f2310ff41563171c5b9ac87a893efe
marko154 <marko.gartnar777@gmail.com>
2025-07-06 14:14:37 +0200

    fixed date parsing for mladina

--- scraper/app/providers/mladina.py
+++ scraper/app/providers/mladina.py
@@ -8,4 +8,6 @@ class MladinaProvider(NewsProvider):
             name="Mladina",
             url="https://www.mladina.si",
             rss_feeds=["https://feeds.feedburner.com/Mladina"],
+            # Thu, 3 Jul 2025 22:00:00 GMT
+            rss_date_format="%a, %d %b %Y %H:%M:%S %Z",
         )
