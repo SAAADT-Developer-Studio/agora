28c3c75afd236db5c05d6583410aff79d97ef520
marko154 <marko.gartnar777@gmail.com>
2025-07-19 23:54:45 +0200

    updated db schema to use modern Mapped and mapped_column methods

--- scraper/app/database/schema.py
+++ scraper/app/database/schema.py
@@ -1,36 +1,39 @@
+from datetime import datetime
+from typing import Optional, List
 from sqlalchemy import (
     create_engine,
-    Column,
-    Integer,
     String,
     Float,
     DateTime,
     ForeignKey,
 )
-from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy.dialects.postgresql import ARRAY
 import app.config as config
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
 
 
 class Article(Base):
     __tablename__ = "article"
-    id = Column(Integer, primary_key=True)
-    url = Column(String, unique=True, nullable=False)
-    title = Column(String)
-    published_at = Column(DateTime(timezone=True))
-    deck = Column(String)
-    summary = Column(String, nullable=True)
-    author = Column(String, nullable=True)
-    content = Column(String, nullable=True)
-    embedding = Column(ARRAY(Float))
 
-    news_provider_key = Column(String, ForeignKey("news_provider.key"), nullable=False)
-    news_provider = relationship("NewsProvider", back_populates="articles")
+    id: Mapped[int] = mapped_column(primary_key=True)
+    url: Mapped[str] = mapped_column(String, unique=True)
+    title: Mapped[str] = mapped_column(String)
+    published_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    deck: Mapped[Optional[str]] = mapped_column(String)
+    summary: Mapped[Optional[str]] = mapped_column(String)
+    author: Mapped[Optional[str]] = mapped_column(String)
+    content: Mapped[Optional[str]] = mapped_column(String)
+    embedding: Mapped[List[float]] = mapped_column(ARRAY(Float))
 
-    cluster_id = Column(Integer, ForeignKey("cluster.id"), nullable=True)
-    cluster = relationship("Cluster", back_populates="articles")
+    news_provider_key: Mapped[str] = mapped_column(String, ForeignKey("news_provider.key"))
+    news_provider: Mapped["NewsProvider"] = relationship("NewsProvider", back_populates="articles")
+
+    cluster_id: Mapped[Optional[int]] = mapped_column(ForeignKey("cluster.id"))
+    cluster: Mapped[Optional["Cluster"]] = relationship("Cluster", back_populates="articles")
 
     def __repr__(self):
         return f"<Article(id={self.id}, url={self.url}, title={self.title})>"
@@ -38,22 +41,24 @@ class Article(Base):
 
 class NewsProvider(Base):
     __tablename__ = "news_provider"
-    key = Column(String, primary_key=True, nullable=False)
-    name = Column(String, unique=True, nullable=False)
-    url = Column(String, unique=True, nullable=False)
 
-    articles = relationship("Article", back_populates="news_provider")
+    key: Mapped[str] = mapped_column(String, primary_key=True)
+    name: Mapped[str] = mapped_column(String, unique=True)
+    url: Mapped[str] = mapped_column(String, unique=True)
+
+    articles: Mapped[List["Article"]] = relationship("Article", back_populates="news_provider")
 
     def __repr__(self):
-        return f"<NewsProvider(id={self.id}, name={self.name}, key={self.key})>"
+        return f"<NewsProvider(key={self.key}, name={self.name})>"
 
 
 class Cluster(Base):
     __tablename__ = "cluster"
-    id = Column(Integer, primary_key=True)
-    title = Column(String, nullable=False)
 
-    articles = relationship("Article", back_populates="cluster")
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String)
+
+    articles: Mapped[List["Article"]] = relationship("Article", back_populates="cluster")
 
     def __repr__(self):
         return f"<Cluster(id={self.id}, title={self.title})>"
