591dabe321c5678b2f1f8a70ec486fbe1a912777
marko154 <marko.gartnar777@gmail.com>
2025-07-23 00:34:26 +0200

    added provider logos, added test that asserts that logos for all providers exist

---
+++ scraper/app/tests/providers/test_providers.py
@@ -0,0 +1,12 @@
+from app.providers.keys import ProviderKey
+import os
+
+
+def test_if_provider_logos_exist():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    logos_path_relative = "../../../data/logos"
+    logos_dir = os.path.join(dir_path, logos_path_relative)
+    logo_filenames = {os.path.splitext(filename)[0] for filename in os.listdir(logos_dir)}
+    for key in ProviderKey:
+        if key.value not in logo_filenames:
+            raise AssertionError(f"Logo for {key.value} does not exist in {logos_dir}")
