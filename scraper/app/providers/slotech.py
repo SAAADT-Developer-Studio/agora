2597d976f8700e5b6eab0c8efd7364002e8d71ad
marko154 <marko.gartnar777@gmail.com>
2025-10-31 21:33:48 +0100

    slotech image parsing

--- scraper/app/providers/slotech.py
+++ scraper/app/providers/slotech.py
@@ -1,4 +1,4 @@
-from app.providers.news_provider import NewsProvider, ArticleMetadata
+from app.providers.news_provider import NewsProvider
 from app.providers.enums import ProviderKey
 
 
@@ -10,3 +10,14 @@ class SloTechProvider(NewsProvider):
             url="https://slo-tech.com",
             rss_feeds=["http://feeds.st.si/ST-novice"],
         )
+
+    def parse_rss_entry_image_urls(self, entry):
+        media_content = entry.get("media_content")
+        if media_content and len(media_content) > 0:
+            image_urls = [
+                item.get("url")
+                for item in media_content
+                if isinstance(item, dict) and "url" in item
+            ]
+            return [url for url in image_urls if isinstance(url, str)]
+        return []
